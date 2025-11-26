a73a1b7615f57c0dee37e1dcf9503654d6e217e3
dasfruit808 <lionelkdeguzman@yahoo.com>
2025-11-26 12:10:40 -0800

    Revamp nav bar and window styling

--- script.js
+++ script.js
@@ -123,8 +123,6 @@ const pilotNameStatEl = document.getElementById('pilot-name-stat');
 const pilotTitleStatEl = document.getElementById('pilot-title-stat');
 const pilotNameStatsPanelEl = document.getElementById('pilot-name-stats-panel');
 const pilotTitleStatsPanelEl = document.getElementById('pilot-title-stats-panel');
-const windowShowDesktopBtn = document.getElementById('window-show-desktop');
-const windowOpenHubBtn = document.getElementById('window-open-hub');
 const windowBackToStartBtn = document.getElementById('window-back-to-start');
 const hubCloseBtn = document.getElementById('hub-close');
 const hubPlayNextBtn = document.getElementById('hub-play-next');
@@ -199,8 +197,6 @@ function setNavigationUnlocked(unlocked) {
     document.body.classList.toggle('controls-locked', !navigationUnlocked);
 
     const lockableButtons = [
-        windowShowDesktopBtn,
-        windowOpenHubBtn,
         windowBackToStartBtn,
         ...uiRibbonButtons
     ];
@@ -213,14 +209,6 @@ function setNavigationUnlocked(unlocked) {
 }
 
 setNavigationUnlocked(false);
-
-function hideOverlaysAndResumeGame() {
-    resumeGameFromFocusLoss({ triggeredByUser: true });
-    hideAllOverlays();
-    if (gameRunning) {
-        gamePaused = false;
-    }
-}
 
 function startFreePlaySession() {
     freePlaySessionActive = true;
@@ -241,9 +229,7 @@ function initializeUIEvents() {
     bindButtonClick(playBtn, startFreePlaySession);
     bindButtonClick(connectBtn, handleConnectButtonClick);
 
-    bindButtonClick(windowOpenHubBtn, showHub);
     bindButtonClick(windowBackToStartBtn, showStartMenu);
-    bindButtonClick(windowShowDesktopBtn, hideOverlaysAndResumeGame);
 
     bindButtonClick(hubCloseBtn, showStartMenu);
     bindButtonClick(hubPlayNextBtn, () => startGame(true));
